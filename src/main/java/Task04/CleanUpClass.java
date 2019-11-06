f7af913116252971c980799388570aefb4d1e543
Stuff <stuff77@mail.ru>
2019-11-06 15:18:51 +0300

    Task 04

--- src/main/java/Task04/CleanUpClass.java
+++ src/main/java/Task04/CleanUpClass.java
@@ -3,29 +3,33 @@ package Task04;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import org.springframework.util.ReflectionUtils;
 
-class CleanUpClass<K> {
+class CleanUpClass {
 
     void cleanup(Object object, Set<String> fieldsToCleanup, Set<String> fieldsToOutput) {
-        if (object instanceof Map) cleanMap(object, fieldsToCleanup, fieldsToOutput);
-        else cleanObject(object, fieldsToCleanup, fieldsToOutput);
+        Set<String> mySet = new HashSet<String>();
+        mySet.addAll(fieldsToCleanup);
+        mySet.addAll(fieldsToOutput);
+        if (object instanceof Map) cleanMap(object, fieldsToCleanup, fieldsToOutput, mySet);
+        else cleanObject(object, fieldsToCleanup, fieldsToOutput,mySet);
     }
 
-    private void cleanMap(Object object, final Set<String> fieldsToCleanup, final Set<String> fieldsToOutput) {
-        Class clazz = object.getClass();
+    private void cleanMap(Object object, final Set<String> fieldsToCleanup, final Set<String> fieldsToOutput, final Set<String> mySet) {
         try {
+            for (String field:mySet){
+                isContains(field,object);
+            }
             for (String key:fieldsToCleanup) {
-                isContains(key,object);
-                Method removeMethod = clazz.getMethod("remove", Object.class);
+                Method removeMethod = object.getClass().getMethod("remove", Object.class);
                 removeMethod.setAccessible(true);
                 removeMethod.invoke(object,key);
             }
             for (String key:fieldsToOutput){
-                isContains(key,object);
-                Method getMethod = clazz.getMethod("get", Object.class);
+                Method getMethod = object.getClass().getMethod("get", Object.class);
                 getMethod.setAccessible(true);
                 System.out.println(getMethod.invoke(object,key));
             }
@@ -44,13 +48,8 @@ class CleanUpClass<K> {
         if (containsKeyMethod.invoke(object,key).equals(false)) {throw new IllegalArgumentException();}
     }
 
-    private void cleanObject(final Object object, final Set<String> fieldsToCleanup, final Set<String> fieldsToOutput) {
-        for (String field:fieldsToCleanup){
-            if (ReflectionUtils.findField(object.getClass(),field) == null) {
-                throw new IllegalArgumentException();
-            }
-        }
-        for (String field:fieldsToOutput){
+    private void cleanObject(final Object object, final Set<String> fieldsToCleanup, final Set<String> fieldsToOutput, final Set<String> mySet) {
+        for (String field:mySet){
             if (ReflectionUtils.findField(object.getClass(),field) == null) {
                 throw new IllegalArgumentException();
             }
